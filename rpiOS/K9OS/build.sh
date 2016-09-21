7b33a06e3804474ce2eab624bb066d849b5aec5c
ioa <ioa@nna.fendersons>
2016-09-21 14:48:11 +0200

    added "custom" linker script

--- rpiOS/K9OS/build.sh
+++ rpiOS/K9OS/build.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
-arm-none-eabi-gcc -O0 -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -nostartfiles -nostdlib -ffreestanding -g k9os.c -o kernel.elf
+arm-none-eabi-gcc -O0 -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -nostartfiles -g -Wl,-verbose -Wl,-T,k9-linker.x k9os.c -o kernel.elf
 
 arm-none-eabi-objcopy kernel.elf -O binary kernel.img
 
